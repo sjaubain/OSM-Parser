d174892c52f53a2f0cdba8ce69e39d4573cf8287
simon <simon.jobin@heig-vd.ch>
2022-01-10 14:08:31 +0100

    testing maven workflow

--- osmparser/src/main/java/heig/osmparser/controllers/MainController.java
+++ osmparser/src/main/java/heig/osmparser/controllers/MainController.java
@@ -115,7 +115,7 @@ public class MainController implements Initializable {
         });
 
         mnitmExportCSV.setOnAction(event -> {
-            log("exporting CSV file to output/ dir", Log.LogLevels.INFO);
+            log("exporting CSV file to output/ directory", Log.LogLevels.INFO);
             CSVConverter.graphToCSV(g);
         });
 
